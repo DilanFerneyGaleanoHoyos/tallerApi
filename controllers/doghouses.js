eaf15cd41df584141889352c414323e23db52b96
Dilan Galeano <dilan.galeano@uptc.edu.co>
2023-12-15 18:53:21 -0500

    first commit1

--- controllers/doghouses.js
+++ controllers/doghouses.js
@@ -1,65 +1,68 @@
 // doghouse.js
-const Doghouse = require('../models/model-doghouses');
-const Dog = require('../models/model-dogs');
+const Band = require('../models/model-doghouses');
+const Member = require('../models/model-dogs');
 
 module.exports = {
     findAll: async (req, res) => {
         try {
-            const data = await Doghouse.find({}).populate('dogs');
-            return res.status(200).json({ "state": true, "data": data });
+            const data = await Band.find({}).populate('members')
+            return res.status(200).json({ "state": true, "data": data })
         } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
+            return res.status(500).json({ "state": false, "error": error })
         }
     },
 
     findByObjectId: async (req, res) => {
-        const { id } = req.params;
+        const { id } = req.params
         try {
-            const data = await Doghouse.findById(id).populate('dogs');
-            return res.status(200).json({ "state": true, "data": data });
+            const data = await Band.findById(id).populate('members')
+            return res.status(200).json({ "state": true, "data": data })
         } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
-        }},
+            return res.status(500).json({ "state": false, "error": error })
+        }
+
+
+    },
 
     findById: async (req, res) => {
-        const { id } = req.params;
+        const { id } = req.params
         try {
-            const data = await Doghouse.find({ id: id }).populate('dogs');
-            return res.status(200).json({ "state": true, "data": data });
+            const data = await Band.find({ id: id }).populate('members')
+            return res.status(200).json({ "state": true, "data": data })
         } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
+            return res.status(500).json({ "state": false, "error": error })
         }
     },
 
     save: async (req, res) => {
-        const doghouse = new Doghouse(req.body);
+        const band = new Band(req.body)
 
         try {
-            const data = await doghouse.save();
-            return res.status(200).json({ "state": true, "data": data });
+            const data = await band.save()
+            return res.status(200).json({ "state": true, "data": data })
         } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
+            return res.status(500).json({ "state": false, "error": error })
         }
     },
 
     update: async (req, res) => {
-        const { id } = req.params;
+        const { id } = req.params
         try {
-            const data = await Doghouse.findByIdAndUpdate(id, req.body);
-            return res.status(200).json({ "state": true, "data": data });
+            const data = await Band.findByIdAndUpdate(id, req.body)
+            return res.status(200).json({ "state": true, "data": data })
         } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
+            return res.status(500).json({ "state": false, "error": error })
         }
     },
 
     remove: async (req, res) => {
-        const { id } = req.params;
+        const { id } = req.params
         try {
-            const data = await Doghouse.findByIdAndDelete(id, req.body);
-            await Dog.deleteMany({ doghouse: id });
-            return res.status(200).json({ "state": true, "data": data });
+            const data = await Band.findByIdAndDelete(id, req.body)
+            await Member.deleteMany({ band: id });
+            return res.status(200).json({ "state":true,"data": data})
         } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
+            return res.status(500).json({ "state": false, "error": error })
         }
     }
-};
+}
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,93 +1,94 @@
 // dogs.js
-const Dog = require('../models/model-dogs');
-const Doghouse = require('../models/model-doghouses');
+const Member = require('../models/model-dogs');
+const Band = require('../models/model-doghouses');
 
-module.exports = {
-  findAll: async (req, res) => {
-    try {
-      const data = await Dog.find({});
-      return res.status(200).json({ "state": true, "data": data });
-    } catch (error) {
-      return res.status(500).json({ "state": false, "error": error });
-    }
-  },
+module.exports={
+    findAll:async(req, res)=>{
+        try{
+            const data = await Member.find({})
+            return res.status(200).json({"state":true, "data":data})
+        }catch(error){
+            return res.status(500).json({"state":false,"error":error})
+        }
+    },
 
-  findByObjectId: async (req, res) => {
-    const { id } = req.params;
-    try {
-      const data = await Dog.findById(id);
-      return res.status(200).json({ "state": true, "data": data });
-    } catch (error) {
-      return res.status(500).json({ "state": false, "error": error });
-    }
-  },
+    findByObjectId:async(req, res)=>{
+        const {id}=req.params
+        try{
+            const data = await Member.findById(id)
+            return res.status(200).json({"state":true, "data":data})
+        }catch(error){
+            return res.status(500).json({"state":false,"error":error})
+        }
+    },
 
-  findById: async (req, res) => {
-    const { id } = req.params;
-    try {
-      const data = await Dog.find({ id: id });
-      return res.status(200).json({ "state": true, "data": data });
-    } catch (error) {
-      return res.status(500).json({ "state": false, "error": error });
-    }
-  },
+    findById:async(req, res)=>{
+        const {id} = req.params
+        try{
+            const data = await Member.find({id:id})
+            return res.status(200).json({"state":true, "data":data})
+        }catch(error){
+            return res.status(500).json({"state":false,"error":error})
+        }
+    },
 
-  save: async (req, res) => {
-    try {
-        const dogHouseId = req.body.doghouses; // Assuming 'doghouses' is provided in the request body
-        const dogHouse = await doghouse.findById(dogHouseId); // Utilizando el modelo Doghouse
-
-        if (dogHouse) {
-            try {
-                const dog = new Dog(req.body);
-                dog.doghouses = dogHouse;
-                const result = await dog.save();
-
-                // Update doghouse with new dog
-                dogHouse.dogs.push(dog);
-                await dogHouse.save();
-
-                return res.status(200).json({ "status": true, "data": result });
-            } catch (error) {
-                console.log(error);
-                return res.status(500).json({ "status": false, "error": error });
+    save: async (req, res) => {
+        const { id } = req.params;
+    
+        try {
+            const band = await Band.findById(id)
+    
+            if (band) {
+                try {
+                    const member = new Member(req.body)
+                    member.band = band
+                    const result = await member.save()
+                    bandObject = band.toObject()
+                    bandObject.members.push(member)
+                    await band.updateOne(bandObject)
+                    return res.status(200).json({ "status": true, "data": result })
+                } catch (error) {
+                    console.log(error)
+                    return res.status(500).json({ "status": false, "error": error })
+                }
+            } else {
+                return res.status(404).json({ "status": false, "error": "La Banda No Existe" })
             }
-        } else {
-            return res.status(404).json({ "status": false, "error": "La casa para perros no existe pweeo" });
+        } catch (error) {
+            return res.status(500).json({ "status": false, "error": "El id esta incompleto" })
         }
-    } catch (error) {
-        return res.status(500).json({ "status": false, "error": "El id está incompleto perro" });
-    }
-},
+    },
 
-
-  update: async (req, res) => {
-    const { id } = req.params;
-    try {
-      const data = await Dog.findByIdAndUpdate(id, req.body);
-      return res.status(200).json({ "state": true, "data": data });
-    } catch (error) {
-      return res.status(500).json({ "state": false, "error": error });
-    }
-  },
-
-  remove: async (req, res) => {
-    const { id } = req.params;
-    try {
-      const dog = await Dog.findById(id);
-      if (dog) {
-        const doghouseId = dog.doghouse;
-        const data = await Dog.findByIdAndDelete(id);
-        if (data) {
-          const doghouse = await Doghouse.findById(doghouseId);
-          doghouse.residents.pull(id);
-          await doghouse.save();
-          return res.status(200).json({ "status": true, "data": data });
-        } else {
-          return res.status(404).json({ "status": false, "error": "Perro no encontrado" });
+    update:async(req, res)=>{
+        const { id } = req.params
+        try{
+            const data = await Member.findByIdAndUpdate(id, req.body)
+            return res.status(200).json({ "state": true, "data": data })
+        }catch(error){
+            return res.status(500).json({"state":false,"error":error})
         }
-      }
-    } catch (error) {
-      return res.status(500).json({ "status": false, "error": error });
+    },
+
+    remove: async (req, res) => {
+        const { id } = req.params
+        try {
+            const member = await Member.findById(id)
+            if (member) {
+                const bandId = member.band
+                const data = await Member.findByIdAndDelete(id)
+                if (data) {
+                    const band = await Band.findById(bandId)
+                    band.members.pull(id)
+                    await band.save()
+                    return res.status(200).json({ "status": true, "data": data })
+                } else {
+                    return res.status(404).json({ "status": false, "error": "Miembro no encontrado" })
+                }
+            } else {
+                return res.status(404).json({ "status": false, "error": "Miembro no encontrado" })
+            }
+        } catch (error) {
+            return res.status(500).json({ "status": false, "error": error })
+        }
     }
-  }};
+}    
