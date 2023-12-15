bc7e9971caac4496f732beefa2f9dee5bbfb5c6a
Dilan Galeano <dilan.galeano@uptc.edu.co>
2023-12-15 17:50:30 -0500

    first commit

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -3,92 +3,81 @@ const Dog = require('../models/model-dogs');
 const Doghouse = require('../models/model-doghouses');
 
 module.exports = {
-    findAll: async (req, res) => {
-        try {
-            const data = await Dog.find({});
-            return res.status(200).json({ "state": true, "data": data });
-        } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
-        }
-    },
-
-    findByObjectId: async (req, res) => {
-        const { id } = req.params;
-        try {
-            const data = await Dog.findById(id);
-            return res.status(200).json({ "state": true, "data": data });
-        } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
-        }
-    },
-
-    findById: async (req, res) => {
-        const { id } = req.params;
-        try {
-            const data = await Dog.find({ id: id });
-            return res.status(200).json({ "state": true, "data": data });
-        } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
-        }
-    },
-
-    save: async (req, res) => {
-        const { id } = req.params;
-    
-        try {
-            const doghouses = await doghouses.findById(id)
-    
-            if (dogs) {
-                try {
-                    const dogs = new Dogs(req.body)
-                    dogs.dogs = dogs
-                    const result = await dogs.save()
-                    dogsObject = dogs.toObject()
-                    dogsObject.dogs.push(dogs)
-                    await dogs.updateOne(dogsObject)
-                    return res.status(200).json({ "status": true, "data": result })
-                } catch (error) {
-                    console.log(error)
-                    return res.status(500).json({ "status": false, "error": error })
-                }
-            } else {
-                return res.status(404).json({ "status": false, "error": "La casa No Existe" })
-            }
-        } catch (error) {
-            return res.status(500).json({ "status": false, "error": "El id esta incompleto" })
-        }
-    },
-
-    update: async (req, res) => {
-        const { id } = req.params;
-        try {
-            const data = await Dog.findByIdAndUpdate(id, req.body);
-            return res.status(200).json({ "state": true, "data": data });
-        } catch (error) {
-            return res.status(500).json({ "state": false, "error": error });
-        }
-    },
-
-    remove: async (req, res) => {
-        const { id } = req.params;
-        try {
-            const dog = await Dog.findById(id);
-            if (dog) {
-                const doghouseId = dog.doghouse;
-                const data = await Dog.findByIdAndDelete(id);
-                if (data) {
-                    const doghouse = await Doghouse.findById(doghouseId);
-                    doghouse.residents.pull(id);
-                    await doghouse.save();
-                    return res.status(200).json({ "status": true, "data": data });
-                } else {
-                    return res.status(404).json({ "status": false, "error": "Perro no encontrado" });
-                }
-            } else {
-                return res.status(404).json({ "status": false, "error": "Perro no encontrado" });
-            }
-        } catch (error) {
-            return res.status(500).json({ "status": false, "error": error });
-        }
+  findAll: async (req, res) => {
+    try {
+      const data = await Dog.find({});
+      return res.status(200).json({ "state": true, "data": data });
+    } catch (error) {
+      return res.status(500).json({ "state": false, "error": error });
     }
-};
+  },
+
+  findByObjectId: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const data = await Dog.findById(id);
+      return res.status(200).json({ "state": true, "data": data });
+    } catch (error) {
+      return res.status(500).json({ "state": false, "error": error });
+    }
+  },
+
+  findById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const data = await Dog.find({ id: id });
+      return res.status(200).json({ "state": true, "data": data });
+    } catch (error) {
+      return res.status(500).json({ "state": false, "error": error });
+    }
+  },
+
+  save: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const doghouses = await Doghouse.findById(id);
+
+      if (doghouses) {
+        const dog = new Dog(req.body);
+        dog.doghouses = doghouses;
+        const result = await dog.save();
+        return res.status(200).json({ "status": true, "data": result });
+      } else {
+        return res.status(404).json({ "status": false, "error": "La casa No Existe" });
+      }
+    } catch (error) {
+      return res.status(500).json({ "status": false, "error": error });
+    }
+  },
+
+  update: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const data = await Dog.findByIdAndUpdate(id, req.body);
+      return res.status(200).json({ "state": true, "data": data });
+    } catch (error) {
+      return res.status(500).json({ "state": false, "error": error });
+    }
+  },
+
+  remove: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const dog = await Dog.findById(id);
+      if (dog) {
+        const doghouseId = dog.doghouse;
+        const data = await Dog.findByIdAndDelete(id);
+        if (data) {
+          const doghouse = await Doghouse.findById(doghouseId);
+          doghouse.residents.pull(id);
+          await doghouse.save();
+          return res.status(200).json({ "status": true, "data": data });
+        } else {
+          return res.status(404).json({ "status": false, "error": "Perro no encontrado" });
+        }
+      }
+    } catch (error) {
+      return res.status(500).json({ "status": false, "error": error });
+    }
+  }};
--- models/model-dogs.js
+++ models/model-dogs.js
@@ -4,28 +4,28 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const DogSchema = new Schema({
-    id: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    breed: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    doghouses: {
-        type: mongoose.Types.ObjectId,
-        ref: 'doghouses'
-    }
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  doghouses: {
+    type: mongoose.Types.ObjectId,
+    ref: 'doghouses'
+  }
 }, {
-    versionKey: false
+  versionKey: false
 });
 module.exports = mongoose.model('dogs', DogSchema);
